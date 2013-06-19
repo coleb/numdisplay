d57580c404843fae3c2f8e3cc0a62a140faf9925
Brian Cole <bcole@gemini.edu>
2013-06-19 10:14:17 -1000

    Initial commit of numdisplay into a git repository.

---
+++ defsetup.py
@@ -0,0 +1,16 @@
+from __future__ import division # confidence high
+
+pkg = "numdisplay"
+
+setupargs = { 
+    'version' :         "1.5",
+    'description' :     "Package for displaying numpy arrays in DS9",
+    'author' :          "Warren Hack",
+    'maintainer_email': "help@stsci.edu",
+    'url' :             "",
+    'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+    'platforms' :       ["any"],
+    'data_files' :      [ ( pkg, ['lib/imtoolrc', 'lib/LICENSE.txt','lib/ichar.dat'] )]
+    }
+
+
